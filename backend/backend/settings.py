c3bd4307d0e4e801b2a550fd94543e7a31174721
UzNik <uznik.99@mail.ru>
2024-02-22 05:57:42 +0400

    'bootstrap' is connected

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,14 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
-STATIC_ROOT = BASE_DIR / 'static/'
+# STATIC_ROOT = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
